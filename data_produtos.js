a54016ff9af25e49672ad9c92dcc99cea5252c57
Bernardo Enock <bernardoenockgm@gmail.com>
2022-06-20 11:24:22 -0300

    feat: ETF Shop

---
+++ data_produtos.js
@@ -0,0 +1,65 @@
+
+// Criação de matrix = [{"tipo": valor,"tipo": valor,"tipo": valor},{"tipo": valor,"tipo": valor,"tipo": valor}]
+const dataProdutos = [
+    {
+        "id":12,
+        "produtoNome": "Camera Canon",
+        "image": "img/canon.jpeg",
+        "preco": "2000.00",
+        "precoPromocional":"1900.00",
+        "oferta":true,
+        "estoque":100,
+        "categoria": "camera"
+    },
+    {
+        "id":20,
+        "produtoNome": "Camera Sony",
+        "image": "img/sonya6000.jpeg",
+        "preco": "3500.00",
+        "precoPromocional":"2500.00",
+        "oferta":true,
+        "estoque":100,
+        "categoria": "camera"
+    },
+    {
+        "id":200,
+        "produtoNome": "Camera Nikon",
+        "image": "img/nikon.jpg",
+        "preco": "1500.00",
+        "precoPromocional":"1000.00",
+        "oferta":true,
+        "estoque":100,
+        "categoria": "camera"
+    },
+    {
+        "id":30,
+        "produtoNome": "Kit Sony",
+        "image": "img/equipSony.jpeg",
+        "preco": "3500.00",
+        "precoPromocional":"2500.00",
+        "oferta":true,
+        "estoque":100,
+        "categoria": "acessorio"
+    },
+    {
+        "id":33,
+        "produtoNome": "Estabilizador",
+        "image": "img/estabilizador.jpg",
+        "preco": "3500.00",
+        "precoPromocional":"2500.00",
+        "oferta":true,
+        "estoque":100,
+        "categoria": "acessorio"
+    },
+    {
+        "id":20,
+        "produtoNome": "Ring Ligth",
+        "image": "img/ringligth.jpg",
+        "preco": "3500.00",
+        "precoPromocional":"2500.00",
+        "oferta":true,
+        "estoque":100,
+        "categoria": "acessorio"
+    }
+    
+]
---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+
+//Retorna uma array com todos os elementos da classe ou id determinado
+const cardProduto = document.querySelectorAll(".vitrineProdutos_vitrine li") 
+//add eventos de click
